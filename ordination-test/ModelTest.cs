04aa44b6a7978d67556109434dc01c29bd4ba784
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-05-15 06:37:33 +0200

    class scaffold for testing various model classes

---
+++ ordination-test/ModelTest.cs
@@ -0,0 +1,25 @@
+﻿namespace ordination_test;
+
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Service;
+using shared.Model;
+
+[TestClass]
+public class ModelTest
+{
+    public DataService ServiceSetup()
+    {
+        DataService service;
+        var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
+        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
+        var context = new OrdinationContext(optionsBuilder.Options);
+        service = new DataService(context);
+        service.SeedData();
+
+        return service;
+    }
+
+    [TestMethod]
+
+}
